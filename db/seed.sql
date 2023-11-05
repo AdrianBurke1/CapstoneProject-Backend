1a370ea4ae2dd15acc771063ce99dff908e0945f
paoAracena <Paolaaracena1233@gmail.com>
2023-11-05 09:35:33 -0500

    seed.sql

--- db/seed.sql
+++
@@ -1,4 +0,0 @@
-\c mindful_dev;
-
-INSERT INTO roadmap (name, username, email,bio, goal1,goal2,goal3,goal4) VALUES
-('Paola','PaoAracena','Pao@gmail.com','this is my bio','visit family','go to dinner with friend','host lunch','call family');
