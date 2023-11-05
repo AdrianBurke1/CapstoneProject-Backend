2cf3ea3134599c3a2774676c348cd654f92611e7
paoAracena <Paolaaracena1233@gmail.com>
2023-11-05 15:17:36 -0500

    added delete and post

--- queries/signup.js
+++ queries/signup.js
@@ -8,10 +8,47 @@ const getAllsignups = async () => {
       return error;
     }
   };
+// ONE USER 
+const getSignup = async (id) => {
+    try {
+      const oneSignup = await db.one("SELECT * FROM signups WHERE id=$1", id);
+      return oneSignup;
+    } catch (error) {
+      return error;
+    }
+  };
 
+  // CREATE -> POST
+  const createSignup = async (signup) => {
+    try {
+        console.log("this is the user", signup)
+      const newSignup = await db.one(
+        "INSERT INTO signups (name, username, bio,category) VALUES($1, $2, $3, $4) RETURNING *",
+        [signup.name, signup.username, signup.bio, signup.category]
+      );
+      return newSignup;
+    } catch (error) {
+      return error;
+    }
+  };
+
+ //DELETE
+ const deleteSignup = async (id) => {
+    try {
+      const deletedSignup = await db.one(
+        "DELETE FROM signups WHERE id = $1 RETURNING *",
+        id
+      );
+      return deletedSignup;
+    } catch (error) {
+      return error;
+    }
+  }; 
 
 
   module.exports = {
     getAllsignups,
-   
+    getSignup,
+    createSignup,
+    deleteSignup,
   };
