b746bb4002ccf8fca978958e80fbf51af8aad530
Ari-So-Irie <arius1500@gmail.com>
2023-11-08 21:47:31 -0500

    faulty prompts

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // DEPENDENCIES
+const cors = require("cors");
 const express = require("express");
 
 // CONFIGURATION
 const app = express();
 
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+const promptsController = require("./controllers/promptsController.js");
+app.use("/prompts", promptsController);
 // ROUTES
 app.get("/", (req, res) => {
   res.send("mindfulMe App");
 });
+
+app.get("*", (req,res) => {
+  res.status(404).send("This is the Error Response")
+})
+
 
 // EXPORT
 module.exports = app;
